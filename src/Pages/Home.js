0f73767499c8407691cc7b8237d136242da4a483
Mostafa Mohsin <mostafamohsin783@gmail.com>
2023-11-05 11:27:51 -0800

    resume Update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
     {/* <p className="lg:text-4xl md:text-3xl max-sm:text-2xl font-bold font-serif ">React Developer</p> */}
       <p className="py-6 font-serif">I am Full stack Developer with React js , Node js,<br /> Express and MongoDB
  basically Frontend specalist. </p>
-      <a target={"_blank"} href="https://drive.google.com/file/d/1-WbvdlyLaazljCHaqIx7I5O-rau8yAUj/view?usp=sharing" className="btn border-none  bg-orange-600 hover:bg-orange-600  hover:text-white max-sm:btn-md btn-light text-lime-100 text-md"> <CgFileDocument className="text-base" /> Resume</a>
+      <a target={"_blank"} href="https://drive.google.com/file/d/1MGDSffjq__xQ1JKVf-aChJ7Ljk70v3Pe/view?usp=sharing" className="btn border-none  bg-orange-600 hover:bg-orange-600  hover:text-white max-sm:btn-md btn-light text-lime-100 text-md"> <CgFileDocument className="text-base" /> Resume</a>
     </div>
   </div>
 </div>
